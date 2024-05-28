4ac9ab363951cc1e8ae8c1567b1ff2049bd4c3d5
Kai <jazztyy@gmail.com>
2024-05-28 17:14:15 +0800

    resolve conflict

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ConfigProvider } from 'antd';
 import { AntdRegistry } from '@ant-design/nextjs-registry';
-import './globals.css';
+import Navbar  from '../components/Navbar';
+import "./globals.css";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -34,7 +35,8 @@ export default function RootLayout({
         }}
       >
         <body className={inter.className}>
-          <AntdRegistry>{children}</AntdRegistry>
+        <Navbar/>
+        <AntdRegistry>{children}</AntdRegistry>
         </body>
       </ConfigProvider>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,42 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Home page
+      <section>
+        <div>
+          <h2>尋找最適合你的課程</h2>
+          <span>讓我們陪著你尋找答案</span> 
+          <input type="text" />
+        </div>
+        <img src="" alt="" />
+      </section>
+      <section>
+        <h3>
+          熱門
+          <span>課程</span>
+          <img src="" alt="" />
+        </h3>
+
+      </section>
+      <section>
+        <h3>
+          尋找最適合你的
+          <span>課程</span>
+          <img src="" alt="" />
+        </h3>
+        <ul>
+          <li></li>
+        </ul>
+      </section>
+      <section>
+        <h3>
+          成為 Panacea 的
+          <span>線上教練</span>
+          <img src="" alt="" />
+        </h3>
+      </section>
+      <section>
+
+      </section>
     </main>
   );
 }
