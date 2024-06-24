c2a412a6157f06c7beda3844a10a0b51072633cd
jazztyy <jazztyy@gmail.com>
2024-06-24 21:55:29 +0800

    chore: api route

--- app/api/auth.ts
+++ app/api/auth.ts
@@ -2,7 +2,7 @@ import { request } from '../../util/request';
 
 export const login = (postData: { email: string; password: string }) =>
   request({
-    url: '/auth/sign-in',
+    url: 'api/auth/sign-in',
     method: 'POST',
     data: postData,
   });
