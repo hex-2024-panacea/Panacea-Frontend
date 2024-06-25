a0741e08be32c0a8b0d8bc71d54661755a28ebbf
Ellis <pp840405@gmail.com>
2024-06-25 18:14:19 +0800

    fix:更新NavBar取得user info方式

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,28 @@
-'use client';
-
 import Image from 'next/image';
-import Cookies from 'js-cookie';
-import { getUserInfo } from '@/app/api/user';
-import { useState, useEffect } from 'react';
-import userStore from '../stores/user';
-
+import Link from 'next/link';
+import { cookies } from 'next/headers';
 interface NavbarOption {
   title: string;
   url: string;
 }
 
-export default function Navbar() {
-  const { setUserInfo } = userStore();
-  const [navbarOptions, setNavbarOptions] = useState<NavbarOption[]>([
+interface UserInfoType {
+  name: string;
+  email: string;
+  isCoach: boolean;
+  isAdmin: boolean;
+}
+
+const getUserInfoHandler = async () => {
+  const cookieStore = cookies();
+  const token = cookieStore.get('token')?.value;
+  let userInfo: UserInfoType = {
+    name: '',
+    email: '',
+    isCoach: false,
+    isAdmin: false,
+  };
+  let navBarData: NavbarOption[] = [
     {
       title: '成為教練',
       url: '/signup',
@@ -26,78 +35,69 @@ export default function Navbar() {
       title: '註冊',
       url: '/signup',
     },
-  ]);
-  const getUserInfoHandler = async () => {
-    const { name: storeName } = userStore.getState();
-
-    if (Cookies.get('token') && !storeName) {
-      const data = await getUserInfo();
-      const { name, email, avatar, isAdmin, isCoach } = data;
-
-      setUserInfo({
-        name,
-        email,
-        avatar,
-        isAdmin,
-        isCoach,
-      });
-
-      if (isCoach) {
-        setNavbarOptions([
-          {
-            title: '刊登課程',
-            url: '/profile/coach/course-manage',
-          },
-          {
-            title: name,
-            url: '/profile',
-          },
-        ]);
-      } else {
-        setNavbarOptions([
-          {
-            title: '成為教練',
-            url: '/apply-coach',
-          },
-          {
-            title: '',
-            url: '/login',
-          },
-          {
-            title: name,
-            url: '/profile',
-          },
-        ]);
-      }
+  ];
+  if (token) {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/user-info`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const projects = await res.json();
+    userInfo = projects.data;
+    if (userInfo.isCoach) {
+      navBarData = [
+        {
+          title: '刊登課程',
+          url: '/profile/coach/course-manage',
+        },
+        {
+          title: userInfo.name,
+          url: '/profile',
+        },
+      ];
+    } else {
+      navBarData = [
+        {
+          title: '成為教練',
+          url: '/apply-coach',
+        },
+        {
+          title: '',
+          url: '/login',
+        },
+        {
+          title: userInfo.name,
+          url: '/profile',
+        },
+      ];
     }
-  };
-
-  useEffect(() => {
-    getUserInfoHandler();
-  }, []);
-
+  }
+  return { token, userInfo, navBarData };
+};
+export default async function Navbar() {
+  const { navBarData } = await getUserInfoHandler();
   return (
     <nav className="fixed left-0 right-0 top-0 z-10 flex items-center justify-center bg-[#FFF] pb-[23px] pt-[30px] shadow-sm">
       <div className="flex w-full max-w-[1296px] items-center justify-between">
         <div className="flex items-center">
-          <a href="/" className="cursor-pointer">
+          <Link href="/" className="cursor-pointer">
             <Image src="/logo.svg" alt="logo" width={200} height={26} className="mr-[60px]" />
-          </a>
-          <a href="/courses" className="cursor-pointer">
+          </Link>
+          <Link href="/courses" className="cursor-pointer">
             課程列表
-          </a>
+          </Link>
         </div>
         <div className="absolute right-[20%] top-0 flex items-center rounded-b-[8px] bg-[#FAFAFA] shadow-sm">
           <ul className="flex items-center gap-[20px] px-[48px] py-[40px]">
-            {navbarOptions.map(({ url, title }, index) => (
+            {navBarData.map(({ url, title }, index) => (
               <li key={index}>
                 {url === '/profile' ? (
-                  <a href={url} className="heading6 flex items-center gap-[4px]">
+                  <Link href={url} className="heading6 flex items-center gap-[4px]">
                     {' '}
                     <Image src="/avatar.svg" width={32} height={32} alt="notify icon" /> {title}{' '}
-                  </a>
+                  </Link>
                 ) : title ? (
-                  <a href={url}>{title}</a>
+                  <Link href={url}>{title}</Link>
                 ) : (
                   <div>
                     <Image src="/notifications.svg" width={20} height={20} alt="notify icon" />
