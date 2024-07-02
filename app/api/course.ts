9371a7d5b438da16268bfc8f632d7328cbcf9f7c
Wen Kai Wu (吳文凱) <wenkai.wu@T-JQT4FHM7N7.local>
2024-07-02 18:04:00 +0800

    fix: api request, course details api and page

--- app/api/course.ts
+++ app/api/course.ts
@@ -1,11 +1,18 @@
 import fetchData from '@/util/request';
 import { ResponesBody } from '@/types/request';
-import { CourseList, CourseListParams } from '@/types/courses';
+import { CourseList, CourseListParams, CourseDetails } from '@/types/courses';
 
 export const getCourseList = async (params: CourseListParams): Promise<ResponesBody<CourseList[]>> => {
   return fetchData({
     url: 'api/course',
     method: 'GET',
     params,
+  });
+};
+
+export const getCourseDetails = async (id: string): Promise<ResponesBody<CourseDetails>> => {
+  return fetchData({
+    url: `api/course/${id}/details`,
+    method: 'GET',
   });
 };
--- app/courses/[id]/page.tsx
+++ app/courses/[id]/page.tsx
@@ -1,45 +1,19 @@
-'use client';
-
+// import { CourseDetails } from '@/types/courses'
+import { getCourseDetails } from '@/app/api/course';
 // import Image from 'next/image';
 import bg from '../../../public/bg-pagetop.svg';
 
-interface CourseDetails {
-  coach: string;
-  coachAvatar: string;
-  courseName: string;
-  courseContent: string;
-  coursePrice: {
-    price: number;
-    count: number;
-  }[];
+interface CourseDetailPageProps {
+  params: {
+    id: string;
+  };
 }
 
-const CourseDetails: CourseDetails[] = [
-  {
-    coach: '測試',
-    coachAvatar:
-      'https://images.unsplash.com/photo-1544717305-2782549b5136?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    courseName: '藝術治療',
-    courseContent:
-      '在感受與真實之間，創作可以是一艘船，接應與連結著我們的心靈。讓我陪你一起透過藝術的創作，跟著心流找到適合自己的一隅。緩緩地，覺察與接納自己獨特的樣貌。',
-    coursePrice: [
-      {
-        price: 200,
-        count: 3,
-      },
-      {
-        price: 300,
-        count: 5,
-      },
-      {
-        price: 400,
-        count: 4,
-      },
-    ],
-  },
-];
+export default async function CourseDetailPage({ params }: CourseDetailPageProps) {
+  console.log(params);
+  const data = await getCourseDetails(params.id);
+  console.log(data);
 
-export default function CourseDetailPage() {
   return (
     <main className="min-h-[100dvh] pt-[80px]">
       <header
